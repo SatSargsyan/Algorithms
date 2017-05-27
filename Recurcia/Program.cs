fb9271b8c0084fc96bbe36948bb387f34f0e06fc
SatSargsyan <satenik.sargsyan2017@gmail.com>
2017-05-27 18:34:28 +0400

    Bary srjel

---
+++ Recurcia/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursia
+{
+    class Program
+    {
+        static int DigitCount(int n)
+        {
+            if(n<9)
+            {
+                return 1;
+
+            }
+            else 
+            {
+                return 1 + DigitCount(n / 10);
+
+            }
+
+        
+        } 
+        static void Main(string[] args)
+        {
+            int n=300;
+            Console.WriteLine(DigitCount(n));
+
+            
+            Console.ReadKey();
+
+        }
+    }
+}
+
---
+++ Recurcia/Recurcia/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recurcia
+{
+    class Program
+    {
+        static void BackWord(string s)
+        {
+            if (s.Length >= 1)
+            {
+                string s1 = s.Substring(0, s.Length-1);
+                Console.Write(s[s.Length-1]);
+                BackWord(s1);
+
+            }
+        }
+        // 2-rd depqum popokhutyuny chi tpvi
+        static void BackWord1(string s)
+        {
+            if (s.Length >= 1)
+            {
+                string s1 = s.Substring(0, s.Length - 1);
+                BackWord1(s1);
+
+                Console.Write(s[s.Length - 1]);
+                
+            }
+        }
+        static void Main(string[] args)
+        {
+            string s = "barev";
+            BackWord(s);
+           
+            Console.WriteLine();
+
+            BackWord1(s);
+            Console.ReadKey();
+        }
+    }
+}
